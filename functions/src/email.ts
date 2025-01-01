ebd5c955f6d7e9a83234094e0c0cfd95bbe2dc73
David Dal Busco <david.dalbusco@outlook.com>
2025-01-01 15:51:55 +0100

    feat: proxy body

--- functions/src/email.ts
+++ functions/src/email.ts
@@ -3,7 +3,6 @@ import {firestore} from "firebase-admin";
 import * as functions from "firebase-functions";
 import DocumentData = firestore.DocumentData;
 
-const mailFrom: string = functions.config().mail.from;
 const resendApiKey: string = functions.config().mail.resend.api_key;
 
 export const sendEmail = async ({
@@ -11,14 +10,7 @@ export const sendEmail = async ({
 }: {
   req: express.Request;
 }): Promise<DocumentData> => {
-  const {to} = body;
-
-  const mailOptions = {
-    from: `Juno <${mailFrom}>`,
-    to: [to],
-    subject: "Monitoring Update: Cycles Deposited to Your Modules",
-    html: "<strong>it works!</strong>",
-  };
+  const {from, to, subject, html} = body;
 
   const response = await fetch("https://api.resend.com/emails", {
     method: "POST",
@@ -26,7 +18,7 @@ export const sendEmail = async ({
       "Content-Type": "application/json",
       Authorization: `Bearer ${resendApiKey}`,
     },
-    body: JSON.stringify(mailOptions),
+    body: JSON.stringify({from, to, subject, html}),
   });
 
   if (!response.ok) {
